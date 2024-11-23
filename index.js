a652b681f92b0eb1c7598c2b2d6ed194699a65e0
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-23 02:56:12 -0500

    progress until 4.3

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import Hello from "./Hello.js"
 import Lab5 from './Lab5/index.js'
 import cors from "cors"
 
+import UserRoutes from "./Kanbas/Users/routes.js";
+
 const app = express()
 app.use(cors());  
 app.use(express.json());
+
+UserRoutes(app);
 
 Hello(app)
 Lab5(app)
