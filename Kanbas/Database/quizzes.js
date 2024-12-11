eb15b397efca1b6c70cfc8f28baab3f1738314a7
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-12-11 14:15:25 -0500

    updates question with published field + new IDs

--- Kanbas/Database/quizzes.js
+++ Kanbas/Database/quizzes.js
@@ -16,7 +16,7 @@ const quizGroup = Object.freeze({
 
 export default [
     {
-        _id: "q0",
+        _id: "7fBmPcYQ6RWT",
         title: "Default Quiz",
         description: "A Very Default Description Here",
         course: "RS101",
@@ -34,9 +34,10 @@ export default [
         dueDate: "2025-05-15T23:59",
         availableDate: "2024-05-15T00:00",
         closeDate: "2025-05-16T23:59", // until date equivalent
+        published: true,
     },
     {
-        _id: "q1",
+        _id: "ZG9X3KnVr5Aq",
         title: "Quiz A",
         description: "A Very Quiz A Description Here",
         course: "RS102",
@@ -54,9 +55,10 @@ export default [
         dueDate: "2025-05-15T23:59",
         availableDate: "2024-05-15T00:00",
         closeDate: "2025-05-16T23:59", // until date equivalent
+        published: true,
     },
     {
-        _id: "q2",
+        _id: "bJ6LtwQF89Xp",
         title: "Quiz B",
         description: "A Very Quiz B Description Here",
         course: "RS103",
@@ -74,5 +76,48 @@ export default [
         dueDate: "2025-05-15T23:59",
         availableDate: "2024-05-15T00:00",
         closeDate: "2025-05-16T23:59", // until date equivalent
+        published: true,
+    },
+    {
+        _id: "K2mYVz7XN34P",
+        title: "Quiz C",
+        description: "A Very C Description Here",
+        course: "RS101",
+        quizType: quizType.GRADEDSURVEY,
+        points: 50,
+        assignmentGroup: quizGroup.QUIZZES,
+        shuffleAnswers: true,
+        timeLimitMin: 60,
+        maxAttempts: 1, // no multiple attempts boolean, just set this value
+        showCorrectAnswers: true,
+        accessCode: "",
+        oneQuestionAtATime: true, // one question at a time
+        webcamRequired: false,
+        lockQuestionAfterAns: false,
+        dueDate: "2025-05-15T23:59",
+        availableDate: "2024-05-15T00:00",
+        closeDate: "2025-05-16T23:59", // until date equivalent
+        published: true,
+    },
+    {
+        _id: "LW9Xp6KJ7YQF",
+        title: "Quiz D",
+        description: "A Very D Description Here",
+        course: "RS102",
+        quizType: quizType.GRADED,
+        points: 100,
+        assignmentGroup: quizGroup.QUIZZES,
+        shuffleAnswers: true,
+        timeLimitMin: 20,
+        maxAttempts: 1, // no multiple attempts boolean, just set this value
+        showCorrectAnswers: false,
+        accessCode: "abcde",
+        oneQuestionAtATime: false, // one question at a time
+        webcamRequired: false,
+        lockQuestionAfterAns: false,
+        dueDate: "2025-05-15T23:59",
+        availableDate: "2024-05-15T00:00",
+        closeDate: "2025-05-16T23:59", // until date equivalent
+        published: false,
     },
 ]
